928f14e7496b3f8781a6e6a475c7f4c6bf0246a0
Joseph Yi <sendtojsph@gmail.com>
2023-03-02 11:55:58 -0500

    Initial Commit

---
+++ CupcakeCornerV5/AddressView.swift
@@ -0,0 +1,45 @@
+//
+//  AddressView.swift
+//  CupcakeCornerV5
+//
+//  Created by Joe Yi on 12/23/22.
+//
+//
+//
+//
+
+import SwiftUI
+
+struct AddressView: View {
+    @ObservedObject var order: Order
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Name", text: $order.name)
+                TextField("Street", text: $order.streetAdress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink {
+                    CheckoutView(order: order)
+                } label: {
+                    Text("Check out")
+                }
+            }
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AddressView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            AddressView(order: Order())
+        }
+    }
+}
---
+++ CupcakeCornerV5/CupcakeCornerV5App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerV5App.swift
+//  CupcakeCornerV5
+//
+//  Created by Joe Yi on 12/23/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerV5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
